12a544757eadd5c40b7da56f2aa23b32a80c3788
Volkan Selcuk <vselcuk@gmail.com>
2024-10-22 16:23:13 +0200

    PLT-977 fixed lint errors and updated examples

--- examples/san/main.tf
+++ examples/san/main.tf
@@ -2,10 +2,7 @@ module "this" {
   source = "../../"
 
   cloudflare_api_token = var.cloudflare_api_token
-
-  zone_name   = var.domain_name
-  domain_name = "test-san.${var.domain_name}"
-
+  domain_name          = "test-san.${var.domain_name}"
   subject_alternative_names = [
     ["subdomain.test-san.${var.domain_name}", "cloudflare"],
     ["test-other.${var.domain_name}", "cloudflare"]
--- locals.tf
+++ locals.tf
@@ -1,10 +1,7 @@
 locals {
   # Creates a map of domains and their dns providers
   domain_validation_providers = {
-    for v in concat([
-      [var.domain_name, var.dns_validation_provider]
-    ], var.subject_alternative_names) : v[0] => {
-      provider = v[1]
+    for v in concat([[var.domain_name, var.dns_validation_provider]], var.subject_alternative_names) : v[0] => { provider = v[1]
       # if condition to check the root domain; flaconi.de vs www.flaconi.de
       zone_domain = length(split(".", v[0])) == 2 ? v[0] : regex("^.+\\.(.+\\..+)$", v[0])[0]
     }
