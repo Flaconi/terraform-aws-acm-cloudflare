66c6a9482890ef7529f1e1313831c1aba5c440b5
Volkan Selcuk <vselcuk@gmail.com>
2025-02-03 18:10:04 +0100

    PLT-1121 fixed provider version
    The latest version (v5) for Cloudflare provider is not compatible with the terraform code.

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 4.41"
+      version = "~> 4.41"
     }
     aws = {
       source  = "hashicorp/aws"
