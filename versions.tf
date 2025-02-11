a1a65a1929398df91dfea3c6e3def9f140dfe8b2
Sergey Novikov <sergey.novikov@flaconi.de>
2025-02-11 18:52:58 +0100

    Pin cloudflare provider version to 4.52

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.51"
+      version = "4.52.0"
     }
     aws = {
       source  = "hashicorp/aws"
