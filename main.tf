df12467a77ef815174e2ec2b9fc8d00fa26cbac7
Sergey Novikov <sergey.novikov@flaconi.de>
2025-08-22 17:21:34 +0200

    OPS-6815 Fix name reference

--- main.tf
+++ main.tf
@@ -34,5 +34,5 @@ resource "aws_acm_certificate_validation" "this" {
 
   certificate_arn = aws_acm_certificate.this.arn
 
-  validation_record_fqdns = cloudflare_dns_record.this.*.name
+  validation_record_fqdns = [for key, record in cloudflare_dns_record.this: record.name]
 }
