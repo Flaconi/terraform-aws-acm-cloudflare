92bfa1714c1fb1aaddcc6ccead896511ed40ea75
Sergey Novikov <sergey.novikov@flaconi.de>
2025-08-27 13:26:11 +0200

    OPS-6815 Remove provider form the module

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "cloudflare_api_token" {
-  description = "The Cloudflare API token."
-  type        = string
-  sensitive   = true
-}
-
 variable "validate_certificate" {
   description = "Whether to validate certificate by creating DNS record"
   type        = bool
